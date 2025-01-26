e99df6ccf004787ed3aa9f90642a330295d05268
Dan Manning <1888089+dmanning23@users.noreply.github.com>
2025-01-26 12:55:09 -0600

    initial commit

---
+++ alien_sprite.asm
@@ -0,0 +1,808 @@
+;alien Sprite Data (Height: 16)
+
+;standing Animation Data:
+alienstanding11
+  .byte %00000000 ; Row 1
+  .byte %01000010 ; Row 2
+  .byte %10000100 ; Row 3
+  .byte %11000000 ; Row 4
+  .byte %01101000 ; Row 5
+  .byte %00101000 ; Row 6
+  .byte %00110000 ; Row 7
+  .byte %01100000 ; Row 8
+  .byte %10100001 ; Row 9
+  .byte %10111011 ; Row 10
+  .byte %10100100 ; Row 11
+  .byte %10011011 ; Row 12
+  .byte %01001010 ; Row 13
+  .byte %00001110 ; Row 14
+  .byte %00111100 ; Row 15
+  .byte %00000000 ; Row 16
+
+alienstanding12
+  .byte %00000000 ; Row 1
+  .byte %00000000 ; Row 2
+  .byte %00000000 ; Row 3
+  .byte %00000100 ; Row 4
+  .byte %00000100 ; Row 5
+  .byte %00000100 ; Row 6
+  .byte %00001000 ; Row 7
+  .byte %00010000 ; Row 8
+  .byte %00000100 ; Row 9
+  .byte %00000100 ; Row 10
+  .byte %00011000 ; Row 11
+  .byte %00000000 ; Row 12
+  .byte %00000001 ; Row 13
+  .byte %00000001 ; Row 14
+  .byte %00000010 ; Row 15
+  .byte %00111100 ; Row 16
+
+alienstandingColor11
+  .byte $C2 ; Row 1
+  .byte $C2 ; Row 2
+  .byte $C2 ; Row 3
+  .byte $C2 ; Row 4
+  .byte $C2 ; Row 5
+  .byte $C2 ; Row 6
+  .byte $C2 ; Row 7
+  .byte $C2 ; Row 8
+  .byte $C2 ; Row 9
+  .byte $C2 ; Row 10
+  .byte $C2 ; Row 11
+  .byte $C2 ; Row 12
+  .byte $C2 ; Row 13
+  .byte $C2 ; Row 14
+  .byte $C2 ; Row 15
+  .byte $C2 ; Row 16
+
+alienstandingColor12
+  .byte $00 ; Row 1
+  .byte $00 ; Row 2
+  .byte $A4 ; Row 3
+  .byte $A4 ; Row 4
+  .byte $A4 ; Row 5
+  .byte $A4 ; Row 6
+  .byte $A4 ; Row 7
+  .byte $A4 ; Row 8
+  .byte $A4 ; Row 9
+  .byte $A4 ; Row 10
+  .byte $A4 ; Row 11
+  .byte $A4 ; Row 12
+  .byte $A4 ; Row 13
+  .byte $A4 ; Row 14
+  .byte $A4 ; Row 15
+  .byte $A4 ; Row 16
+
+alienstanding21
+  .byte %00000000 ; Row 1
+  .byte %01000010 ; Row 2
+  .byte %10000100 ; Row 3
+  .byte %11000000 ; Row 4
+  .byte %01101000 ; Row 5
+  .byte %00101000 ; Row 6
+  .byte %00110000 ; Row 7
+  .byte %01100000 ; Row 8
+  .byte %11100100 ; Row 9
+  .byte %10111100 ; Row 10
+  .byte %10000000 ; Row 11
+  .byte %10011011 ; Row 12
+  .byte %11001010 ; Row 13
+  .byte %11001110 ; Row 14
+  .byte %00111100 ; Row 15
+  .byte %00000000 ; Row 16
+
+alienstanding22
+  .byte %00000000 ; Row 1
+  .byte %00000000 ; Row 2
+  .byte %00000000 ; Row 3
+  .byte %00000100 ; Row 4
+  .byte %00000100 ; Row 5
+  .byte %00000100 ; Row 6
+  .byte %00001000 ; Row 7
+  .byte %00010000 ; Row 8
+  .byte %00000010 ; Row 9
+  .byte %00000010 ; Row 10
+  .byte %00011100 ; Row 11
+  .byte %00000000 ; Row 12
+  .byte %00000001 ; Row 13
+  .byte %00000001 ; Row 14
+  .byte %00000010 ; Row 15
+  .byte %00111100 ; Row 16
+
+alienstandingColor21
+  .byte $D2 ; Row 1
+  .byte $C2 ; Row 2
+  .byte $C2 ; Row 3
+  .byte $C2 ; Row 4
+  .byte $C2 ; Row 5
+  .byte $C2 ; Row 6
+  .byte $C2 ; Row 7
+  .byte $C2 ; Row 8
+  .byte $C2 ; Row 9
+  .byte $C2 ; Row 10
+  .byte $C2 ; Row 11
+  .byte $C2 ; Row 12
+  .byte $C2 ; Row 13
+  .byte $C2 ; Row 14
+  .byte $C2 ; Row 15
+  .byte $D2 ; Row 16
+
+alienstandingColor22
+  .byte $00 ; Row 1
+  .byte $00 ; Row 2
+  .byte $A4 ; Row 3
+  .byte $A4 ; Row 4
+  .byte $A4 ; Row 5
+  .byte $A4 ; Row 6
+  .byte $A4 ; Row 7
+  .byte $A4 ; Row 8
+  .byte $A4 ; Row 9
+  .byte $A4 ; Row 10
+  .byte $A4 ; Row 11
+  .byte $A4 ; Row 12
+  .byte $A4 ; Row 13
+  .byte $A4 ; Row 14
+  .byte $A4 ; Row 15
+  .byte $A4 ; Row 16
+
+alienstanding31
+  .byte %00000000 ; Row 1
+  .byte %01000010 ; Row 2
+  .byte %10000100 ; Row 3
+  .byte %01100100 ; Row 4
+  .byte %00101000 ; Row 5
+  .byte %00101000 ; Row 6
+  .byte %00110000 ; Row 7
+  .byte %00100100 ; Row 8
+  .byte %01100100 ; Row 9
+  .byte %11111000 ; Row 10
+  .byte %10011011 ; Row 11
+  .byte %10011110 ; Row 12
+  .byte %10001110 ; Row 13
+  .byte %10111100 ; Row 14
+  .byte %10000000 ; Row 15
+  .byte %00000000 ; Row 16
+
+alienstanding32
+  .byte %00000000 ; Row 1
+  .byte %00000000 ; Row 2
+  .byte %00000000 ; Row 3
+  .byte %00000000 ; Row 4
+  .byte %00000100 ; Row 5
+  .byte %00000100 ; Row 6
+  .byte %00001000 ; Row 7
+  .byte %00010001 ; Row 8
+  .byte %00000001 ; Row 9
+  .byte %00000110 ; Row 10
+  .byte %00000000 ; Row 11
+  .byte %00000001 ; Row 12
+  .byte %00000001 ; Row 13
+  .byte %00000010 ; Row 14
+  .byte %00111100 ; Row 15
+  .byte %00000000 ; Row 16
+
+alienstandingColor31
+  .byte $D2 ; Row 1
+  .byte $C2 ; Row 2
+  .byte $C2 ; Row 3
+  .byte $C2 ; Row 4
+  .byte $C2 ; Row 5
+  .byte $C2 ; Row 6
+  .byte $C2 ; Row 7
+  .byte $C2 ; Row 8
+  .byte $C2 ; Row 9
+  .byte $C2 ; Row 10
+  .byte $C2 ; Row 11
+  .byte $C2 ; Row 12
+  .byte $C2 ; Row 13
+  .byte $C2 ; Row 14
+  .byte $C2 ; Row 15
+  .byte $D2 ; Row 16
+
+alienstandingColor32
+  .byte $00 ; Row 1
+  .byte $00 ; Row 2
+  .byte $A4 ; Row 3
+  .byte $A4 ; Row 4
+  .byte $A4 ; Row 5
+  .byte $A4 ; Row 6
+  .byte $A4 ; Row 7
+  .byte $A4 ; Row 8
+  .byte $A4 ; Row 9
+  .byte $A4 ; Row 10
+  .byte $A4 ; Row 11
+  .byte $A4 ; Row 12
+  .byte $A4 ; Row 13
+  .byte $A4 ; Row 14
+  .byte $A4 ; Row 15
+  .byte $00 ; Row 16
+
+alienstanding41
+  .byte %00000000 ; Row 1
+  .byte %01000010 ; Row 2
+  .byte %10000100 ; Row 3
+  .byte %11000000 ; Row 4
+  .byte %01101000 ; Row 5
+  .byte %00101000 ; Row 6
+  .byte %00110000 ; Row 7
+  .byte %01100001 ; Row 8
+  .byte %11101011 ; Row 9
+  .byte %10110000 ; Row 10
+  .byte %10011011 ; Row 11
+  .byte %01011110 ; Row 12
+  .byte %01001110 ; Row 13
+  .byte %10111100 ; Row 14
+  .byte %00000000 ; Row 15
+  .byte %00000000 ; Row 16
+
+alienstanding42
+  .byte %00000000 ; Row 1
+  .byte %00000000 ; Row 2
+  .byte %00000000 ; Row 3
+  .byte %00000100 ; Row 4
+  .byte %00000100 ; Row 5
+  .byte %00000100 ; Row 6
+  .byte %00001000 ; Row 7
+  .byte %00010100 ; Row 8
+  .byte %00000100 ; Row 9
+  .byte %00001000 ; Row 10
+  .byte %00000000 ; Row 11
+  .byte %00000001 ; Row 12
+  .byte %00000001 ; Row 13
+  .byte %00000010 ; Row 14
+  .byte %00111100 ; Row 15
+  .byte %00000000 ; Row 16
+
+alienstandingColor41
+  .byte $D2 ; Row 1
+  .byte $C2 ; Row 2
+  .byte $C2 ; Row 3
+  .byte $C2 ; Row 4
+  .byte $C2 ; Row 5
+  .byte $C2 ; Row 6
+  .byte $C2 ; Row 7
+  .byte $C2 ; Row 8
+  .byte $C2 ; Row 9
+  .byte $C2 ; Row 10
+  .byte $C2 ; Row 11
+  .byte $C2 ; Row 12
+  .byte $C2 ; Row 13
+  .byte $C2 ; Row 14
+  .byte $C2 ; Row 15
+  .byte $D2 ; Row 16
+
+alienstandingColor42
+  .byte $00 ; Row 1
+  .byte $00 ; Row 2
+  .byte $A4 ; Row 3
+  .byte $A4 ; Row 4
+  .byte $A4 ; Row 5
+  .byte $A4 ; Row 6
+  .byte $A4 ; Row 7
+  .byte $A4 ; Row 8
+  .byte $A4 ; Row 9
+  .byte $A4 ; Row 10
+  .byte $A4 ; Row 11
+  .byte $A4 ; Row 12
+  .byte $A4 ; Row 13
+  .byte $A4 ; Row 14
+  .byte $A4 ; Row 15
+  .byte $00 ; Row 16
+
+alienstandingSpeed:
+  .byte 11 ; Animation speed
+
+alienstandingFrames:
+  .byte 4 ; Number of frames
+
+;walk Animation Data:
+alienwalk11
+  .byte %00000000 ; Row 1
+  .byte %01000001 ; Row 2
+  .byte %10000010 ; Row 3
+  .byte %11000000 ; Row 4
+  .byte %01100000 ; Row 5
+  .byte %00101100 ; Row 6
+  .byte %00110000 ; Row 7
+  .byte %01100000 ; Row 8
+  .byte %10100001 ; Row 9
+  .byte %10111011 ; Row 10
+  .byte %10100100 ; Row 11
+  .byte %10011011 ; Row 12
+  .byte %01001010 ; Row 13
+  .byte %00001110 ; Row 14
+  .byte %00111100 ; Row 15
+  .byte %00000000 ; Row 16
+
+alienwalk12
+  .byte %00000000 ; Row 1
+  .byte %00000000 ; Row 2
+  .byte %00000000 ; Row 3
+  .byte %00000010 ; Row 4
+  .byte %00000010 ; Row 5
+  .byte %00000010 ; Row 6
+  .byte %00001100 ; Row 7
+  .byte %00010000 ; Row 8
+  .byte %00000100 ; Row 9
+  .byte %00000100 ; Row 10
+  .byte %00011000 ; Row 11
+  .byte %00000000 ; Row 12
+  .byte %00000001 ; Row 13
+  .byte %00000001 ; Row 14
+  .byte %00000010 ; Row 15
+  .byte %00111100 ; Row 16
+
+alienwalkColor11
+  .byte $C2 ; Row 1
+  .byte $C2 ; Row 2
+  .byte $C2 ; Row 3
+  .byte $C2 ; Row 4
+  .byte $C2 ; Row 5
+  .byte $C2 ; Row 6
+  .byte $C2 ; Row 7
+  .byte $C2 ; Row 8
+  .byte $C2 ; Row 9
+  .byte $C2 ; Row 10
+  .byte $C2 ; Row 11
+  .byte $C2 ; Row 12
+  .byte $C2 ; Row 13
+  .byte $C2 ; Row 14
+  .byte $C2 ; Row 15
+  .byte $C2 ; Row 16
+
+alienwalkColor12
+  .byte $00 ; Row 1
+  .byte $A4 ; Row 2
+  .byte $A4 ; Row 3
+  .byte $A4 ; Row 4
+  .byte $A4 ; Row 5
+  .byte $A4 ; Row 6
+  .byte $A4 ; Row 7
+  .byte $A4 ; Row 8
+  .byte $A4 ; Row 9
+  .byte $A4 ; Row 10
+  .byte $A4 ; Row 11
+  .byte $A4 ; Row 12
+  .byte $A4 ; Row 13
+  .byte $A4 ; Row 14
+  .byte $A4 ; Row 15
+  .byte $A4 ; Row 16
+
+alienwalk21
+  .byte %00000000 ; Row 1
+  .byte %00000100 ; Row 2
+  .byte %00001000 ; Row 3
+  .byte %10000000 ; Row 4
+  .byte %01110000 ; Row 5
+  .byte %00011000 ; Row 6
+  .byte %00110000 ; Row 7
+  .byte %01100000 ; Row 8
+  .byte %11100010 ; Row 9
+  .byte %10110010 ; Row 10
+  .byte %10101000 ; Row 11
+  .byte %10011011 ; Row 12
+  .byte %11001010 ; Row 13
+  .byte %00001110 ; Row 14
+  .byte %00111100 ; Row 15
+  .byte %00000000 ; Row 16
+
+alienwalk22
+  .byte %00000000 ; Row 1
+  .byte %00000000 ; Row 2
+  .byte %00000000 ; Row 3
+  .byte %00001000 ; Row 4
+  .byte %00000100 ; Row 5
+  .byte %00000100 ; Row 6
+  .byte %00001000 ; Row 7
+  .byte %00010000 ; Row 8
+  .byte %00000100 ; Row 9
+  .byte %00001100 ; Row 10
+  .byte %00010000 ; Row 11
+  .byte %00000000 ; Row 12
+  .byte %00000001 ; Row 13
+  .byte %00000001 ; Row 14
+  .byte %00000010 ; Row 15
+  .byte %00111100 ; Row 16
+
+alienwalkColor21
+  .byte $C2 ; Row 1
+  .byte $C2 ; Row 2
+  .byte $C2 ; Row 3
+  .byte $C2 ; Row 4
+  .byte $C2 ; Row 5
+  .byte $C2 ; Row 6
+  .byte $C2 ; Row 7
+  .byte $C2 ; Row 8
+  .byte $C2 ; Row 9
+  .byte $C2 ; Row 10
+  .byte $C2 ; Row 11
+  .byte $C2 ; Row 12
+  .byte $C2 ; Row 13
+  .byte $C2 ; Row 14
+  .byte $C2 ; Row 15
+  .byte $C2 ; Row 16
+
+alienwalkColor22
+  .byte $00 ; Row 1
+  .byte $A4 ; Row 2
+  .byte $A4 ; Row 3
+  .byte $A4 ; Row 4
+  .byte $A4 ; Row 5
+  .byte $A4 ; Row 6
+  .byte $A4 ; Row 7
+  .byte $A4 ; Row 8
+  .byte $A4 ; Row 9
+  .byte $A4 ; Row 10
+  .byte $A4 ; Row 11
+  .byte $A4 ; Row 12
+  .byte $A4 ; Row 13
+  .byte $A4 ; Row 14
+  .byte $A4 ; Row 15
+  .byte $A4 ; Row 16
+
+alienwalk31
+  .byte %00000000 ; Row 1
+  .byte %00010000 ; Row 2
+  .byte %00100000 ; Row 3
+  .byte %00100000 ; Row 4
+  .byte %00101100 ; Row 5
+  .byte %00001100 ; Row 6
+  .byte %00101000 ; Row 7
+  .byte %00100000 ; Row 8
+  .byte %01000110 ; Row 9
+  .byte %11111100 ; Row 10
+  .byte %10110000 ; Row 11
+  .byte %10011011 ; Row 12
+  .byte %11001010 ; Row 13
+  .byte %11001110 ; Row 14
+  .byte %00111100 ; Row 15
+  .byte %00000000 ; Row 16
+
+alienwalk32
+  .byte %00000000 ; Row 1
+  .byte %00000000 ; Row 2
+  .byte %00000000 ; Row 3
+  .byte %00010000 ; Row 4
+  .byte %00010000 ; Row 5
+  .byte %00010000 ; Row 6
+  .byte %00010000 ; Row 7
+  .byte %00010000 ; Row 8
+  .byte %00100000 ; Row 9
+  .byte %00000011 ; Row 10
+  .byte %00001100 ; Row 11
+  .byte %00000000 ; Row 12
+  .byte %00000001 ; Row 13
+  .byte %00000001 ; Row 14
+  .byte %00000010 ; Row 15
+  .byte %00111100 ; Row 16
+
+alienwalkColor31
+  .byte $C2 ; Row 1
+  .byte $C2 ; Row 2
+  .byte $C2 ; Row 3
+  .byte $C2 ; Row 4
+  .byte $C2 ; Row 5
+  .byte $C2 ; Row 6
+  .byte $C2 ; Row 7
+  .byte $C2 ; Row 8
+  .byte $C2 ; Row 9
+  .byte $C2 ; Row 10
+  .byte $C2 ; Row 11
+  .byte $C2 ; Row 12
+  .byte $C2 ; Row 13
+  .byte $C2 ; Row 14
+  .byte $C2 ; Row 15
+  .byte $C2 ; Row 16
+
+alienwalkColor32
+  .byte $00 ; Row 1
+  .byte $00 ; Row 2
+  .byte $A4 ; Row 3
+  .byte $A4 ; Row 4
+  .byte $A4 ; Row 5
+  .byte $A4 ; Row 6
+  .byte $A4 ; Row 7
+  .byte $A4 ; Row 8
+  .byte $A4 ; Row 9
+  .byte $A4 ; Row 10
+  .byte $A4 ; Row 11
+  .byte $A4 ; Row 12
+  .byte $A4 ; Row 13
+  .byte $A4 ; Row 14
+  .byte $A4 ; Row 15
+  .byte $A4 ; Row 16
+
+alienwalk41
+  .byte %00000000 ; Row 1
+  .byte %01000001 ; Row 2
+  .byte %10000010 ; Row 3
+  .byte %11000010 ; Row 4
+  .byte %00100010 ; Row 5
+  .byte %00001110 ; Row 6
+  .byte %00101100 ; Row 7
+  .byte %00100000 ; Row 8
+  .byte %01000100 ; Row 9
+  .byte %11111100 ; Row 10
+  .byte %10111000 ; Row 11
+  .byte %10010011 ; Row 12
+  .byte %10001010 ; Row 13
+  .byte %11001110 ; Row 14
+  .byte %10111100 ; Row 15
+  .byte %00000000 ; Row 16
+
+alienwalk42
+  .byte %00000000 ; Row 1
+  .byte %00000000 ; Row 2
+  .byte %00000000 ; Row 3
+  .byte %00000000 ; Row 4
+  .byte %01000000 ; Row 5
+  .byte %00100000 ; Row 6
+  .byte %00010000 ; Row 7
+  .byte %00010000 ; Row 8
+  .byte %00100001 ; Row 9
+  .byte %00000011 ; Row 10
+  .byte %00000100 ; Row 11
+  .byte %00001000 ; Row 12
+  .byte %00000001 ; Row 13
+  .byte %00000001 ; Row 14
+  .byte %00000010 ; Row 15
+  .byte %00111100 ; Row 16
+
+alienwalkColor41
+  .byte $C2 ; Row 1
+  .byte $C2 ; Row 2
+  .byte $C2 ; Row 3
+  .byte $C2 ; Row 4
+  .byte $C2 ; Row 5
+  .byte $C2 ; Row 6
+  .byte $C2 ; Row 7
+  .byte $C2 ; Row 8
+  .byte $C2 ; Row 9
+  .byte $C2 ; Row 10
+  .byte $C2 ; Row 11
+  .byte $C2 ; Row 12
+  .byte $C2 ; Row 13
+  .byte $C2 ; Row 14
+  .byte $C2 ; Row 15
+  .byte $C2 ; Row 16
+
+alienwalkColor42
+  .byte $00 ; Row 1
+  .byte $00 ; Row 2
+  .byte $A4 ; Row 3
+  .byte $A4 ; Row 4
+  .byte $A4 ; Row 5
+  .byte $A4 ; Row 6
+  .byte $A4 ; Row 7
+  .byte $A4 ; Row 8
+  .byte $A4 ; Row 9
+  .byte $A4 ; Row 10
+  .byte $A4 ; Row 11
+  .byte $A4 ; Row 12
+  .byte $A4 ; Row 13
+  .byte $A4 ; Row 14
+  .byte $A4 ; Row 15
+  .byte $A4 ; Row 16
+
+alienwalk51
+  .byte %00000000 ; Row 1
+  .byte %00001100 ; Row 2
+  .byte %00001000 ; Row 3
+  .byte %00001000 ; Row 4
+  .byte %10010100 ; Row 5
+  .byte %00001100 ; Row 6
+  .byte %00101000 ; Row 7
+  .byte %00100000 ; Row 8
+  .byte %01000100 ; Row 9
+  .byte %01111000 ; Row 10
+  .byte %10110000 ; Row 11
+  .byte %10011011 ; Row 12
+  .byte %10001010 ; Row 13
+  .byte %10001110 ; Row 14
+  .byte %10111100 ; Row 15
+  .byte %10000000 ; Row 16
+
+alienwalk52
+  .byte %00000000 ; Row 1
+  .byte %00000000 ; Row 2
+  .byte %00000000 ; Row 3
+  .byte %00000000 ; Row 4
+  .byte %01100000 ; Row 5
+  .byte %00010000 ; Row 6
+  .byte %00010000 ; Row 7
+  .byte %00010000 ; Row 8
+  .byte %00100010 ; Row 9
+  .byte %00000110 ; Row 10
+  .byte %00001000 ; Row 11
+  .byte %00000000 ; Row 12
+  .byte %00000001 ; Row 13
+  .byte %00000001 ; Row 14
+  .byte %00000010 ; Row 15
+  .byte %00111100 ; Row 16
+
+alienwalkColor51
+  .byte $C2 ; Row 1
+  .byte $C2 ; Row 2
+  .byte $C2 ; Row 3
+  .byte $C2 ; Row 4
+  .byte $C2 ; Row 5
+  .byte $C2 ; Row 6
+  .byte $C2 ; Row 7
+  .byte $C2 ; Row 8
+  .byte $C2 ; Row 9
+  .byte $C2 ; Row 10
+  .byte $C2 ; Row 11
+  .byte $C2 ; Row 12
+  .byte $C2 ; Row 13
+  .byte $C2 ; Row 14
+  .byte $C2 ; Row 15
+  .byte $C2 ; Row 16
+
+alienwalkColor52
+  .byte $00 ; Row 1
+  .byte $00 ; Row 2
+  .byte $A4 ; Row 3
+  .byte $A4 ; Row 4
+  .byte $A4 ; Row 5
+  .byte $A4 ; Row 6
+  .byte $A4 ; Row 7
+  .byte $A4 ; Row 8
+  .byte $A4 ; Row 9
+  .byte $A4 ; Row 10
+  .byte $A4 ; Row 11
+  .byte $A4 ; Row 12
+  .byte $A4 ; Row 13
+  .byte $A4 ; Row 14
+  .byte $A4 ; Row 15
+  .byte $A4 ; Row 16
+
+alienwalk61
+  .byte %00000000 ; Row 1
+  .byte %00010000 ; Row 2
+  .byte %00100000 ; Row 3
+  .byte %00110000 ; Row 4
+  .byte %00001100 ; Row 5
+  .byte %00010000 ; Row 6
+  .byte %00110000 ; Row 7
+  .byte %01100000 ; Row 8
+  .byte %01100010 ; Row 9
+  .byte %01111010 ; Row 10
+  .byte %01110000 ; Row 11
+  .byte %11001011 ; Row 12
+  .byte %10001010 ; Row 13
+  .byte %10001110 ; Row 14
+  .byte %10111100 ; Row 15
+  .byte %00000000 ; Row 16
+
+alienwalk62
+  .byte %00000000 ; Row 1
+  .byte %00000000 ; Row 2
+  .byte %00000000 ; Row 3
+  .byte %00000000 ; Row 4
+  .byte %00110000 ; Row 5
+  .byte %00001100 ; Row 6
+  .byte %00001000 ; Row 7
+  .byte %00010000 ; Row 8
+  .byte %00000100 ; Row 9
+  .byte %00000100 ; Row 10
+  .byte %00001000 ; Row 11
+  .byte %00010000 ; Row 12
+  .byte %00000001 ; Row 13
+  .byte %00000001 ; Row 14
+  .byte %00000010 ; Row 15
+  .byte %00111100 ; Row 16
+
+alienwalkColor61
+  .byte $C2 ; Row 1
+  .byte $C2 ; Row 2
+  .byte $C2 ; Row 3
+  .byte $C2 ; Row 4
+  .byte $C2 ; Row 5
+  .byte $C2 ; Row 6
+  .byte $C2 ; Row 7
+  .byte $C2 ; Row 8
+  .byte $C2 ; Row 9
+  .byte $C2 ; Row 10
+  .byte $C2 ; Row 11
+  .byte $C2 ; Row 12
+  .byte $C2 ; Row 13
+  .byte $C2 ; Row 14
+  .byte $C2 ; Row 15
+  .byte $C2 ; Row 16
+
+alienwalkColor62
+  .byte $00 ; Row 1
+  .byte $00 ; Row 2
+  .byte $A4 ; Row 3
+  .byte $A4 ; Row 4
+  .byte $A4 ; Row 5
+  .byte $A4 ; Row 6
+  .byte $A4 ; Row 7
+  .byte $A4 ; Row 8
+  .byte $A4 ; Row 9
+  .byte $A4 ; Row 10
+  .byte $A4 ; Row 11
+  .byte $A4 ; Row 12
+  .byte $A4 ; Row 13
+  .byte $A4 ; Row 14
+  .byte $A4 ; Row 15
+  .byte $A4 ; Row 16
+
+alienwalk71
+  .byte %00000000 ; Row 1
+  .byte %00100000 ; Row 2
+  .byte %01000000 ; Row 3
+  .byte %00110100 ; Row 4
+  .byte %00010010 ; Row 5
+  .byte %00010100 ; Row 6
+  .byte %00111000 ; Row 7
+  .byte %01100000 ; Row 8
+  .byte %11100001 ; Row 9
+  .byte %10111011 ; Row 10
+  .byte %10100100 ; Row 11
+  .byte %10011011 ; Row 12
+  .byte %10001010 ; Row 13
+  .byte %10001110 ; Row 14
+  .byte %00111100 ; Row 15
+  .byte %00000000 ; Row 16
+
+alienwalk72
+  .byte %00000000 ; Row 1
+  .byte %00000000 ; Row 2
+  .byte %00000000 ; Row 3
+  .byte %00000000 ; Row 4
+  .byte %00000000 ; Row 5
+  .byte %00000010 ; Row 6
+  .byte %00000110 ; Row 7
+  .byte %00011000 ; Row 8
+  .byte %00000100 ; Row 9
+  .byte %00000100 ; Row 10
+  .byte %00011000 ; Row 11
+  .byte %00000000 ; Row 12
+  .byte %00000001 ; Row 13
+  .byte %00000001 ; Row 14
+  .byte %00000010 ; Row 15
+  .byte %00111100 ; Row 16
+
+alienwalkColor71
+  .byte $C2 ; Row 1
+  .byte $C2 ; Row 2
+  .byte $C2 ; Row 3
+  .byte $C2 ; Row 4
+  .byte $C2 ; Row 5
+  .byte $C2 ; Row 6
+  .byte $C2 ; Row 7
+  .byte $C2 ; Row 8
+  .byte $C2 ; Row 9
+  .byte $C2 ; Row 10
+  .byte $C2 ; Row 11
+  .byte $C2 ; Row 12
+  .byte $C2 ; Row 13
+  .byte $C2 ; Row 14
+  .byte $C2 ; Row 15
+  .byte $C2 ; Row 16
+
+alienwalkColor72
+  .byte $00 ; Row 1
+  .byte $00 ; Row 2
+  .byte $A4 ; Row 3
+  .byte $A4 ; Row 4
+  .byte $A4 ; Row 5
+  .byte $A4 ; Row 6
+  .byte $A4 ; Row 7
+  .byte $A4 ; Row 8
+  .byte $A4 ; Row 9
+  .byte $A4 ; Row 10
+  .byte $A4 ; Row 11
+  .byte $A4 ; Row 12
+  .byte $A4 ; Row 13
+  .byte $A4 ; Row 14
+  .byte $A4 ; Row 15
+  .byte $A4 ; Row 16
+
+alienwalkSpeed:
+  .byte 7 ; Animation speed
+
+alienwalkFrames:
+  .byte 7 ; Number of frames
+
