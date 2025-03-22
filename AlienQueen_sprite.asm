be0f794d50470643a62041bba352f8a2c2862065
Dan Manning <1888089+dmanning23@users.noreply.github.com>
2025-03-22 14:11:12 -0500

    added alien queen

--- AlienQueen_sprite.asm
+++ AlienQueen_sprite.asm
@@ -53,61 +53,9 @@ AlienQueenStanding12
   .byte %11100100 ; Row 23 (right)
   .byte %00000000 ; Row 24 (right)
 
-AlienQueenStanding21
-  .byte %00000000 ; Row 1 (left)
-  .byte %00100100 ; Row 2 (left)
-  .byte %00011000 ; Row 3 (left)
-  .byte %00110000 ; Row 4 (left)
-  .byte %00110000 ; Row 5 (left)
-  .byte %00110000 ; Row 6 (left)
-  .byte %00010000 ; Row 7 (left)
-  .byte %00011000 ; Row 8 (left)
-  .byte %00001110 ; Row 9 (left)
-  .byte %00000011 ; Row 10 (left)
-  .byte %00000011 ; Row 11 (left)
-  .byte %00001111 ; Row 12 (left)
-  .byte %00010001 ; Row 13 (left)
-  .byte %00100011 ; Row 14 (left)
-  .byte %01000001 ; Row 15 (left)
-  .byte %01000110 ; Row 16 (left)
-  .byte %01000000 ; Row 17 (left)
-  .byte %01100111 ; Row 18 (left)
-  .byte %00100000 ; Row 19 (left)
-  .byte %00010000 ; Row 20 (left)
-  .byte %00010000 ; Row 21 (left)
-  .byte %00010001 ; Row 22 (left)
-  .byte %00100111 ; Row 23 (left)
-  .byte %00000000 ; Row 24 (left)
-
-AlienQueenStanding22
-  .byte %00000000 ; Row 1 (right)
-  .byte %00010010 ; Row 2 (right)
-  .byte %00001110 ; Row 3 (right)
-  .byte %00011000 ; Row 4 (right)
-  .byte %00110000 ; Row 5 (right)
-  .byte %01100000 ; Row 6 (right)
-  .byte %10000000 ; Row 7 (right)
-  .byte %01000000 ; Row 8 (right)
-  .byte %00110000 ; Row 9 (right)
-  .byte %00110000 ; Row 10 (right)
-  .byte %01100010 ; Row 11 (right)
-  .byte %11000001 ; Row 12 (right)
-  .byte %10000111 ; Row 13 (right)
-  .byte %10111100 ; Row 14 (right)
-  .byte %10100000 ; Row 15 (right)
-  .byte %11100110 ; Row 16 (right)
-  .byte %11111110 ; Row 17 (right)
-  .byte %01101110 ; Row 18 (right)
-  .byte %00001100 ; Row 19 (right)
-  .byte %00111000 ; Row 20 (right)
-  .byte %01110000 ; Row 21 (right)
-  .byte %11100000 ; Row 22 (right)
-  .byte %10000000 ; Row 23 (right)
-  .byte %00000000 ; Row 24 (right)
-
 AlienQueenStandingSpeed:
   .byte 30 ; Animation speed
 
 AlienQueenStandingFrames:
-  .byte 2 ; Number of frames
+  .byte 1 ; Number of frames
 
