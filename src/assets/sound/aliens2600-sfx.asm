6a92a02c22df2018627937d630492f402d137cbd
Dan Manning <1888089+dmanning23@users.noreply.github.com>
2025-05-02 22:25:46 -0500

    added knockback sound fx

--- src/assets/sound/aliens2600-sfx.asm
+++ src/assets/sound/aliens2600-sfx.asm
@@ -1,6 +1,6 @@
 SFX_F:
-    .byte 0, 11, 11, 11, 11, 11, 11, 11, 11, 11, 7, 7, 7, 7, 7, 7, 7, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 13, 13, 13, 16, 16, 11, 13, 13, 13, 16, 16, 16, 16, 16 ; LoaderStep
     .byte 0, 30, 28, 26, 24, 22, 20, 18, 16, 14, 12, 10, 8, 6, 4, 4, 4, 4, 4, 4, 4, 4 ; Shoot
+    .byte 0, 11, 11, 11, 11, 11, 11, 11, 11, 11, 7, 7, 7, 7, 7, 7, 7, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 13, 13, 13, 16, 16, 11, 13, 13, 13, 16, 16, 16, 16, 16 ; LoaderStep
     .byte 0, 12, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0, 31, 30, 29, 28, 27, 26, 25, 24, 23, 22, 21, 20 ; LoaderAttack
     .byte 0, 14, 14, 14, 14, 14, 14, 14, 14, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 24, 24 ; LoaderBeginAttack
     .byte 0, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 21, 20, 19, 18, 17, 16, 15, 14, 13, 12, 12, 11, 11, 9, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0 ; LoaderKnockback
@@ -14,10 +14,10 @@ SFX_Fcount = * -SFX_F
     endif
     
 SFX_CV:
-    .byte 0,$23, $24, $25, $26, $27, $27, $27, $27, $27, $27, $26, $26, $20, $20, $26, $26, $f6, $f6, $f6, $f6, $f6, $f6, $f6, $f6, $f6, $f6, $f6, $f6, $f6, $f6, $f6, $f6, $f6, $f6, $f6, $f6, $f5, $f5, $f6, $f6, $f6, $f6, $f5, $f5, $f4, $f3, $f2 ; LoaderStep
-sfxLOADERSTEP = *-SFX_CV-1
     .byte 0,$62, $64, $65, $66, $67, $88, $89, $8a, $8b, $8d, $8e, $8f, $8f, $8f, $8f, $8f, $8f, $8f, $8f, $8f, $8f ; Shoot
 sfxSHOOT = *-SFX_CV-1
+    .byte 0,$23, $24, $25, $26, $27, $27, $27, $27, $27, $27, $26, $26, $20, $20, $26, $26, $f6, $f6, $f6, $f6, $f6, $f6, $f6, $f6, $f6, $f6, $f6, $f6, $f6, $f6, $f6, $f6, $f6, $f6, $f6, $f6, $f5, $f5, $f6, $f6, $f6, $f6, $f5, $f5, $f4, $f3, $f2 ; LoaderStep
+sfxLOADERSTEP = *-SFX_CV-1
     .byte 0,$81, $81, $83, $85, $87, $89, $8b, $8d, $8f, $8f, $8f, $8f, $8f, $8f, $8f, $8f, $8f, $8f, $8f, $8f, $8f, $8f, $8f, $8f, $8f, $8f ; LoaderAttack
 sfxLOADERATTACK = *-SFX_CV-1
     .byte 0,$fe, $fe, $fe, $fe, $fe, $fe, $fe, $fd, $fc, $fc, $fc, $fc, $fc, $fc, $fc, $fc, $fc, $fc, $fc, $fc, $fc ; LoaderBeginAttack
@@ -31,8 +31,9 @@ sfxALIENDEATH = *-SFX_CV-1
 
 
 ; Sound effect lengths for looping
-SFX_LOADERSTEP_LENGTH = #47
+SFX_QUEENSTEP_LENGTH = #20
 SFX_SHOOT_LENGTH = #21
+SFX_LOADERSTEP_LENGTH = #47
 SFX_LOADERATTACK_LENGTH = #26
 SFX_LOADERBEGINATTACK_LENGTH = #21
 SFX_LOADERKNOCKBACK_LENGTH = #35
